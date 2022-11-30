372c6f75287383fd1b02551a2b7400604bfdf2a5
xwq <xiaowenqiang@chengfayun.com>
2022-11-30 15:31:27 +0800

    init

---
+++ .rescriptsrc.js
@@ -0,0 +1,25 @@
+
+module.exports = {
+  webpack: (config) => {
+    config.output.library = `ReactMicroApp`;
+    config.output.libraryTarget = 'umd';
+    config.output.jsonpFunction = `webpackJsonp_ReactMicroApp`;
+    config.output.globalObject = 'window';
+
+    return config;
+  },
+
+  devServer: (_) => {
+    const config = _;
+
+    config.headers = {
+      'Access-Control-Allow-Origin': '*',
+    };
+    config.historyApiFallback = true;
+    config.hot = false;
+    config.watchContentBase = false;
+    config.liveReload = false;
+
+    return config;
+  },
+};
